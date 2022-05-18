ca5ec2dd4e20e47c566df700165031941951e7ed
Ezee1323 <casalipineda.ezequiel@gmail.com>
2022-05-18 16:31:24 -0300

    buscador

---
+++ src/Web/Controllers/BuscadorController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+
+using Web.Models;
+using Web.Models.Enums;
+using Web.Models.Contextos;
+
+namespace Web.Controllers
+{
+    [Authorize]
+    public class BuscadorController : Controller
+    {
+        private readonly ContextoBase _context;
+
+        public BuscadorController(ContextoBase context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["Ingredientes"] = new SelectList(_context.Ingredientes, "Id", "Descripcion");
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [AllowAnonymous]
+        public async Task<IActionResult> Index(List<int> ingredientes)
+        {
+            List <Receta> ListaDeRecetas = new List<Receta>();
+           
+            foreach (var ingrediente in ingredientes)
+            {
+                List<IngredienteReceta> ingRec = _context.IngredientesRecetas
+                    .Include(x => x.Receta)
+                    .Where(x => x.IngredienteId == ingrediente).ToList();
+                ListaDeRecetas.Add(ingRec.Select(x => x.Receta).FirstOrDefault());
+            }
+            ViewData["Recetas"] = ListaDeRecetas;
+            return View();
+        }
+
+    }  
+}
